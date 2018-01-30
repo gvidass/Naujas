040d8ce310ba021383d8129b6f31dbd8430a24ee
siaubalas <siaubalas@gmail.com>
2018-01-30 09:16:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/moksleivis/naujas/Naujas.java
@@ -0,0 +1,13 @@
+package com.example.moksleivis.naujas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Naujas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_naujas);
+    }
+}
